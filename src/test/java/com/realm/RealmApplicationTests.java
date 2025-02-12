038ae08ebe4e8ee578e33aa7dac8b0062c1f0727
nyccreator <andyfernandeznyc@gmail.com>
2025-02-11 19:05:12 -0500

    initial commit

---
+++ src/test/java/com/realm/RealmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.realm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
