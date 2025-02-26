cb55e260dafc5f1337914fcbc957df198378b341
pwuxun <pwu320@gatech.edu>
2025-02-25 18:13:48 -0600

    added basic Document Repository based on ticket specifications

---
+++ src/main/java/com/realm/repository/DocumentRepository.java
@@ -0,0 +1,16 @@
+package com.realm.repository;
+
+import com.realm.entity.Document;
+import org.springframework.data.neo4j.repository.Neo4jRepository;
+
+
+
+public interface DocumentRepository extends Neo4jRepository<Document, Long> {
+    
+    @Query(
+            "MATCH (d:Document) WHERE id(d) = $id " +
+            "MATCH (p:Document) WHERE id(p) = $newParentId " +
+            "MERGE (d)-[:BELONGS_TO]->(p) " + 
+            "RETURN d")
+    public Mono<Void> updateDocumentParent(Long id, Long newParentId);
+}
