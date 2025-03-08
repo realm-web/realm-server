ffc8a78fa231ea61ba428d83ad26c7b878d7558e
Andrey Fernandez <andyfernandeznyc@gmail.com>
2025-03-08 06:37:07 -0500

    Update RealmApplication.java

--- src/main/java/com/realm/RealmApplication.java
+++ src/main/java/com/realm/RealmApplication.java
@@ -1,15 +1,13 @@
 package com.realm;
 
-import com.realm.folder.*;
-import com.realm.document.*;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class RealmApplication {
+
 	public static void main(String[] args) {
-		ApplicationContext context = SpringApplication.run(RealmApplication.class, args);
+		SpringApplication.run(RealmApplication.class, args);
 	}
+
 }
